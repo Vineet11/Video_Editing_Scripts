2e44445c3cafe8d08831d75924ee12db739eef1e
Vineet <vineetmgh@gmail.com>
2022-03-30 14:51:01 +0530

    Refactoring part 3: Moving to method based design
    - Class based design can be used if we want to really scale up this.
    - For now, method based design is simple and clean.

--- VideoEditorUtils.py
+++ VideoEditorUtils.py
@@ -7,17 +7,25 @@ from typing import Any
 import cv2
 import numpy as np
 from scipy import stats
-from moviepy import *
+# from moviepy import *
 from moviepy.editor import VideoFileClip
 from moviepy.editor import AudioFileClip
 
 debug_print = False
+force_attach_audio = False
 
 # Configure args parameters by using the user arguments.
 # We allow overriding most of the default parameters but their default values can change on some of the user passed flags
 
-## Util / helper functions
-# TODO: Add Time string to seconds converter
+## Generic Util / helper functions
+def stringToTime_s(time_str):
+    '''Function takes input string as HH:MM:SS format and converts it as time in seconds'''
+    split_str = time_str.split(":")
+    time_in_seconds = 0
+    for t in split_str:
+        # Going from hour --> seconds
+        time_in_seconds = time_in_seconds*60 + int(t)
+    return time_in_seconds
 
 def getReferenceImage(video_capture, time_s, image_path):
     ref_taskbar_image = video_capture.get_frame(time_s)
@@ -27,6 +35,8 @@ def getReferenceImage(video_capture, time_s, image_path):
         # Load the reference file png or jpg. in BGR format
         ref_taskbar_image = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
     return ref_taskbar_image
+
+### Video editor helper functions
 
 ## Function to detect mask
 def detectTaskbarAndApplyMask(frame, ref_taskbar_image, args):
@@ -68,67 +78,81 @@ def detectTaskbarAndApplyMask(frame, ref_taskbar_image, args):
         edited_frame = frame
     return edited_frame
 
-## Video editor class
-class VideoEditor_v1():
-    def __init__(self, input_args) -> None:
-        self.args = input_args
+def reconfigureArgs(args):
+    assignValue = lambda in_val, default: default if in_val is None else in_val
+    getTime = lambda in_val:  None if in_val is None else stringToTime_s(in_val)
+    # OS dependent default parameters. Can be overwritten if passed by user
+    if args.input_video_OS == 10:
+        args.mask_offset = assignValue(args.mask_offset, 0)
+        args.mask_border_left = assignValue(args.mask_border_left, 0)
+        args.mask_border_right = assignValue(args.mask_border_right, None)
+    elif args.input_video_OS == 7:
+        args.mask_offset = assignValue(args.mask_offset, 8)
+        args.mask_border_left = assignValue(args.mask_border_left, 43)
+        args.mask_border_right = assignValue(args.mask_border_right, -45)
 
-    def reconfigureArgs(self):
-        # Assigning to a shorter variable for usage simplicity
-        args = self.args
-  
-        assignValue = lambda in_val, default: default if in_val is None else in_val
-        if args.input_video_OS == 10:
-            args.mask_offset = assignValue(args.mask_offset, 0)
-            args.mask_border_left = assignValue(args.mask_border_left, 0)
-            args.mask_border_right = assignValue(args.mask_border_right, None)
-        elif args.input_video_OS == 7:
-            args.mask_offset = assignValue(args.mask_offset, 8)
-            args.mask_border_left = assignValue(args.mask_border_left, 43)
-            args.mask_border_right = assignValue(args.mask_border_right, -45)
+    # Time parameters will be passed as strings in the format HH:MM:SS or MM:SS or SS
+    args.reference_frame_time = getTime(args.reference_frame_time)
+    args.start_time = getTime(args.start_time)
+    args.end_time = getTime(args.end_time)
+    # Convert them to seconds
+    # TODO: Convert all time args to seconds
+    # Reassign edited args
+    return args
 
-        # Time parameters will be passed as strings in the format HH:MM:SS or MM:SS or SS
-        # Convert them to seconds
-        # TODO: Convert all time args to seconds
-        # Reassign edited args
-        self.args = args
+def maskBottomTaskbar(get_frame, t, ref_taskbar_image, args):
+    if debug_print:
+        print("Procssing time ={}".format(t))
+    return detectTaskbarAndApplyMask(frame=get_frame(t), ref_taskbar_image=ref_taskbar_image, args=args)
 
-    def maskBottomTaskbar(self, get_frame, t):
-        if debug_print:
-            print("Procssing time ={}".format(t))
-        frame = get_frame(t)
-        return detectTaskbarAndApplyMask(frame=frame, ref_taskbar_image=self.ref_taskbar_image, args=self.args)
-    
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        self.reconfigureArgs()
+def VideoEditor_v10(args):
+    # Modify default values of some of the arguments based on input arguments
+    args = reconfigureArgs(args)
 
-        ### Create a video capture object, in this case we are reading the video from a file
-        input_video_path = self.args.input_video_path
-        vid_capture = VideoFileClip(input_video_path)
+    ### Create a video capture object, in this case we are reading the video from a file
+    input_video_path = args.input_video_path
+    # moviepy video object
+    vid_capture = VideoFileClip(input_video_path)
+    if force_attach_audio:
         audio_capture = AudioFileClip(input_video_path)
         # Set the audio to video
         vid_capture = vid_capture.set_audio(audio_capture)
-        # Print info for user
-        print("Length of the video(in seconds) = {}, fps = {}".format(vid_capture.duration, vid_capture.fps))
-        fps = vid_capture.fps
-        video_length = vid_capture.duration
 
-        ### Get the reference image
-        self.ref_taskbar_image = getReferenceImage(vid_capture, self.args.reference_frame_time_s, self.args.ref_image_path)
-        
-        ### Getting the subclip of the video that we want to work with
-        start_time = np.clip(self.args.start_time, a_min=0, a_max=video_length)
-        end_time = np.clip(self.args.end_time, a_min=0, a_max=video_length)
-        print("Creating a clip from {} to {}".format(start_time, end_time))
+    # Print info for user
+    print("Length of the video(in seconds) = {}, fps = {}".format(vid_capture.duration, vid_capture.fps))
+    fps = vid_capture.fps
+    video_length = vid_capture.duration
 
-        output_video_path = input_video_path[:-4]+"_edited_{}_{}".format(int(start_time), int(end_time))+ ".mp4"
-        vid_capture = vid_capture.subclip(start_time, end_time)
+    ### Get the reference image
+    ref_taskbar_image = getReferenceImage(vid_capture, args.reference_frame_time, args.ref_image_path)
+    
+    ### Getting the subclip of the video that we want to work with
+    assignDefValue = lambda in_val, default: default if in_val is None else in_val
+    args.start_time = assignDefValue(args.start_time, 0) # Init to 0 if none
+    args.end_time = assignDefValue(args.end_time, 10**10) # Init to very high val if none
 
-        # This calls the passed function on every frame. We will do the required processing there
-        edited_video = vid_capture.fl(self.maskBottomTaskbar)
-        edited_video.write_videofile(output_video_path, codec = "libx264", fps=fps)
+    start_time = np.clip(args.start_time, a_min=0, a_max=video_length)
+    end_time = np.clip(args.end_time, a_min=0, a_max=video_length)
+    print("Creating a clip from {} to {}".format(start_time, end_time))
 
-        vid_capture.close()
+    output_video_path = input_video_path[:-4]+"_edited_{}_{}".format(int(start_time), int(end_time))+ ".mp4"
+    vid_capture = vid_capture.subclip(start_time, end_time)
+
+    # This calls the passed function on every frame.
+    # fl function requires the signature get_frame, t. 
+    # We are using the lambda function to call required function using fl
+    edited_video = vid_capture.fl(lambda get_frame, t: maskBottomTaskbar(get_frame, t, ref_taskbar_image, args))
+    edited_video.write_videofile(output_video_path, codec = "libx264", fps=fps)
+
+    vid_capture.close()
+    if force_attach_audio:
         audio_capture.close()
-        edited_video.close()
+    edited_video.close()
 
+def VideoEditor(args, version=1.0):
+    if version==1.0:
+        VideoEditor_v10(args=args)
+
+if __name__=="__main__":
+    print('Testing utils')
+    # stringToTime_s("1:25")
--- edit_video.py
+++ edit_video.py
@@ -9,11 +9,14 @@ Created on Tue Oct 12 21:50:49 2021
 import argparse
 argParser = argparse.ArgumentParser()
 argParser.add_argument('--input_video_path', type=str, required=True, help="Input path of video to be edited")
-argParser.add_argument('--reference_frame_time_s', type=int, required=True, help="in seconds. This frame will be used to extract the taskbar dimension")
+argParser.add_argument('--reference_frame_time', type=str, required=True, help="in seconds. This frame will be used to extract the taskbar dimension")
+
+argParser.add_argument('--mode', type=str, default="editing", choices=["editing"], help="Different modes for the editor. Calibration mode to be added")
 argParser.add_argument('--input_video_OS', type=int, default=10, choices=[7,10],
                         help="Windows OS version on which the video was recorded. This will impact mask and taskbar parameters configuration")
-argParser.add_argument('--start_time', type=int, default=0, help="Time to begin the editing from the provided video")
-argParser.add_argument('--end_time', type=int, default=10**10, help="Time to end the editing from the provided video")
+
+argParser.add_argument('--start_time', type=str, default=None, help="Time to begin the editing from the provided video")
+argParser.add_argument('--end_time', type=str, default=None, help="Time to end the editing from the provided video")
 
 argParser.add_argument('--taskbar_height', type=int, default=40, help="")
 argParser.add_argument('--match_width', type=int, default=60, help="")
@@ -29,7 +32,6 @@ argParser.add_argument('--ref_image_path', type=str, default='', help="Input pat
 args = argParser.parse_args()
 
 # import VideoEditor utils
-from VideoEditorUtils import VideoEditor_v1
-
-video_editor = VideoEditor_v1(args)
-video_editor()
+from VideoEditorUtils import VideoEditor
+# Call video editor.
+VideoEditor(args, version=1.0)
